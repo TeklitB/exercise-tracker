7526572ba5b703325542ed13921b9511a9a53b8e
teklit <teklit@student.chalmers.se>
2020-06-05 10:30:34 +0200

    folders renamed

---
+++ exercise-tracker-backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+//const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+//creates our express server
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors()); //midelware
+app.use(express.json()); //parses the json since the server is going to sending and receiving
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
